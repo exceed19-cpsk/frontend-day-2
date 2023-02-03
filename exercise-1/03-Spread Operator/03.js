095f423cf78b5548b6ba98e82072fa1fd1093408
KanonKC <kanonkce@gmail.com>
2023-02-03 14:26:44 +0700

    Spreading Op for Function

--- exercise-1/03-Spread Operator/03.js
+++ exercise-1/03-Spread Operator/03.js
@@ -20,3 +20,7 @@ console.log(new_item3)
 let new_item4 = {price: 999.99,...item}
 console.log(new_item4)
 // ทำไมค่า price จึงยังมีราคาเท่าเดิมกับ item ตอนแรก ทั้งที่เราใส่ 999.99 ไปแล้ว
+
+let new_item5 = {...item,stock: item.stock-1}
+console.log(new_item5)
+// ทำไมถึงได้ผลลัพธ์เช่นนั้น
---
+++ exercise-1/03-Spread Operator/05.js
@@ -0,0 +1,17 @@
+/*
+อันนี้ของแถมละกันน เนื่องจากเราก็สามารถใช้ Spread Operater Function ได้ด้วย
+ทำให้เราอาจจะกำหนด Function ที่มีจำนวน Parameter ที่ไม่แน่นอนได้ 
+*/
+
+function getSum(...numbers){
+    let total = 0
+    for(var i=0;i<numbers.length;i++){
+        total += numbers[i]
+    }
+    return total
+}
+
+console.log(getSum(1))
+console.log(getSum(10,20,30))
+console.log(getSum(1,2,3,4,5,6,7,8,9,10))
+// คิดว่า numbers จะเก็บข้อมูลเป็น Type อะไร
